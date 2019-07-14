e1071a6ced6b1f5f661104d75851201eacf3facb
Justin LeFebvre <jstnlefebvre@gmail.com>
2019-07-14 19:48:27 -0400

    add a scaffolded server impl

--- src/server_main.rs
+++ src/server_main.rs
@@ -1,4 +1,37 @@
+use amethyst::{
+    core::frame_limiter::FrameRateLimitStrategy,
+    network::{
+        NetworkSimulationTimeSystem,
+        laminar::{LaminarNetworkBundle, LaminarNetworkResource}
+    },
+    Application,
+    GameDataBuilder,
+    GameData,
+    SimpleState,
+    StateData
+};
 
 fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    // Probably want to get this from a command line arg.
+    let socket_addr = "0.0.0.0:23440".parse()?;
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(LaminarNetworkBundle)?
+        .with(NetworkSimulationTimeSystem, "network_sim_time", &[]);
+
+    let mut game = Application::build("./", ServerState)?
+        .with_frame_limit(FrameRateLimitStrategy::Sleep, 60)
+        .with_resource(LaminarNetworkResource::new(socket_addr))
+        .build(game_data)?;
+
+    game.run();
     Ok(())
+}
+
+pub struct ServerState;
+
+impl SimpleState for ServerState {
+    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {}
 }
