ee0e95dda0657caa6b014e7957a624de221a4f7e
Kumar Ankur <kankur@mobileiron.com>
2019-07-21 23:12:52 +0530

    added files required to load map tmx file

---
+++ assets/tilemaps/background.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="background_new" tilewidth="416" tileheight="176" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="416" height="176" source="../textures/truss.png"/>
+ </tile>
+ <tile id="1">
+  <image width="192" height="176" source="../textures/background.png"/>
+ </tile>
+</tileset>
